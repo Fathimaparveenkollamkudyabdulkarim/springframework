f22ca1fa33befc086ee67ae7dd89425bd49236ca
parvin <parveenfathima386@gmail.com>
2025-05-21 04:50:43 +0530

    Initial commit

---
+++ src/test/java/pl/edu/visula/springframework/SpringframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.visula.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
